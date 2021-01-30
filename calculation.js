3cd36779f0540589223efa15b45387d9eb0a6c4a
Abul Bashar <adulbashar124@gmail.com>
2021-01-30 16:12:42 +0600

    calculation part is done

--- calculation.js
+++ calculation.js
@@ -1,27 +1,44 @@
 
 document.getElementById('firstClass-plus-btn').addEventListener('click', function () {
-    clickAction(true, 'firstClass-ticket');
+    clickAction(true, captureAndConvertNum('firstClass-ticket'), 'firstClass-ticket');
+    
 })
 document.getElementById('firstClass-minus-btn').addEventListener('click', function () {
-    clickAction(false, 'firstClass-ticket');
+    clickAction(false, captureAndConvertNum('firstClass-ticket'), 'firstClass-ticket');
 })
 document.getElementById('economy-plus-btn').addEventListener('click', function () {
-    clickAction(true, 'economy-ticket');
+    clickAction(true, captureAndConvertNum('economy-ticket'), 'economy-ticket');
 })
 document.getElementById('economy-minus-btn').addEventListener('click', function () {
-    clickAction(false, 'economy-ticket');
+    clickAction(false, captureAndConvertNum('economy-ticket'), 'economy-ticket');
 })
 
-function clickAction(increase, inputId) {
-    var captureTicket = document.getElementById(inputId).value;
-    var captureTicketNumbers = parseFloat(captureTicket);
-    var newTicketNumbers = captureTicketNumbers;
+function clickAction(increase, CaptureInput, inputId) {
+    var newTicketNumbers = CaptureInput;
     if (increase == false && newTicketNumbers > 0) {
-        var newTicketNumbers = captureTicketNumbers - 1;
+        var newTicketNumbers = CaptureInput - 1;
     }
     else if (increase == true) {
-        var newTicketNumbers = captureTicketNumbers + 1;
+        var newTicketNumbers = CaptureInput + 1;
     }
     document.getElementById(inputId).value = newTicketNumbers;
-}
 
+    var TotalFirstClassTicket = captureAndConvertNum('firstClass-ticket');
+    var costForFirstClassTicket = TotalFirstClassTicket * 150;
+    
+    var TotalEconomyTicket = captureAndConvertNum('economy-ticket');
+    var costForEconomyTicket = TotalEconomyTicket * 100;
+    var subtotalAmount = costForFirstClassTicket + costForEconomyTicket;
+    document.getElementById('subtotal-id').innerText = subtotalAmount;
+
+    var VAT = subtotalAmount * 10/100;
+    document.getElementById('vat-id').innerText = VAT;
+
+    var TotalAmount = subtotalAmount + VAT;
+    document.getElementById('total-id').innerText = TotalAmount;
+}
+function captureAndConvertNum(id) {
+    var captureValue = document.getElementById(id).value;
+    var convertValueNumber = parseInt(captureValue);
+    return convertValueNumber;
+}
