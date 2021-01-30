5eaaeb713b8a3f1e18ac6d32ba5fa366e73ff9b2
Abul Bashar <adulbashar124@gmail.com>
2021-01-30 13:33:41 +0600

    first class and economy ticket count is done

--- calculation.js
+++ calculation.js
@@ -1,32 +1,27 @@
 
-    document.getElementById('plus-btn').addEventListener('click', function(){
-        clickAction(true);
-    })
-    document.getElementById('minus-btn').addEventListener('click', function(){
-        clickAction(false);
-    })
+document.getElementById('firstClass-plus-btn').addEventListener('click', function () {
+    clickAction(true, 'firstClass-ticket');
+})
+document.getElementById('firstClass-minus-btn').addEventListener('click', function () {
+    clickAction(false, 'firstClass-ticket');
+})
+document.getElementById('economy-plus-btn').addEventListener('click', function () {
+    clickAction(true, 'economy-ticket');
+})
+document.getElementById('economy-minus-btn').addEventListener('click', function () {
+    clickAction(false, 'economy-ticket');
+})
 
-function clickAction(increase) {
-    var captureTicket = document.getElementById('firstClass-ticket').value;
-        var captureTicketNumbers = parseFloat(captureTicket);
-        var newTicketNumbers = captureTicketNumbers;
-        if(increase == false && newTicketNumbers>0){
-            var newTicketNumbers = captureTicketNumbers - 1;
-        }
-        else if(increase == true){
-            var newTicketNumbers = captureTicketNumbers + 1;
-        }
-        document.getElementById('firstClass-ticket').value = newTicketNumbers;
+function clickAction(increase, inputId) {
+    var captureTicket = document.getElementById(inputId).value;
+    var captureTicketNumbers = parseFloat(captureTicket);
+    var newTicketNumbers = captureTicketNumbers;
+    if (increase == false && newTicketNumbers > 0) {
+        var newTicketNumbers = captureTicketNumbers - 1;
     }
-document.getElementById('plus-btn').addEventListener('click', function(){
-    var captureTicket = document.getElementById('firstClass-ticket').value;
-    var captureTicketNumbers = parseFloat(captureTicket);
-    var newTicketNumbers = captureTicketNumbers + 1;
-    document.getElementById('firstClass-ticket').value = newTicketNumbers;
-})
-document.getElementById('minus-btn').addEventListener('click', function(){
-    var captureTicket = document.getElementById('firstClass-ticket').value;
-    var captureTicketNumbers = parseFloat(captureTicket);
-    var newTicketNumbers = captureTicketNumbers - 1;
-    document.getElementById('firstClass-ticket').value = newTicketNumbers;
-})
+    else if (increase == true) {
+        var newTicketNumbers = captureTicketNumbers + 1;
+    }
+    document.getElementById(inputId).value = newTicketNumbers;
+}
+
